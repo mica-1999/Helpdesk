1a682c9924d3120bf536dff8dd811d061bb1581e
Micael Ribeiro <micael1999work@gmail.com>
2025-10-24 08:44:23 +0100

    login

--- lib/auth.ts
+++ lib/auth.ts
@@ -49,6 +49,20 @@ export const authOptions: NextAuthOptions = {
     session: {
         strategy: "jwt",
     },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (session.user) {
+                session.user.id = token.id as string;
+            }
+            return session;
+        },
+    },
     pages: {
         signIn: "/login",
     },
